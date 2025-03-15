169e32b7d371845b90dfef29fb13175bc88d82c5
Walid <walid.bensalah20@gmail.com>
2025-03-15 15:52:15 +0100

    ingredient search style

--- scripts/utils/filters.js
+++ scripts/utils/filters.js
@@ -1,23 +1,43 @@
 export const displayFilterIngredients = (recipesData) => {
-    
     const ingredientContainer = document.querySelector(".filter-ingredients");
 
     const buttonIngredients = document.createElement("button");
-    buttonIngredients.classList.add("button-ingredients")
-    buttonIngredients.id = ("button-filter");
+    buttonIngredients.classList.add("button-ingredients");
+    buttonIngredients.id = "button-filter";
     buttonIngredients.textContent = "Ingrédients";
 
-    const ingredientsList = document.createElement("ul");
-    buttonIngredients.appendChild(ingredientsList)
+    const ingredientsSearch = document.createElement("div"); //le bloc qu'il faudra cacher
+    ingredientsSearch.setAttribute("class", "ingredients-search");
+    buttonIngredients.appendChild(ingredientsSearch);
 
-    recipesData.forEach(recipes => {
-        recipes.ingredients.forEach(ingredients => {
-            const ingredientsLi = document.createElement("li");
-            ingredientsLi.textContent = ingredients.ingredient;
-            ingredientsList.appendChild(ingredientsLi); 
+    const searchBarAndGlass = document.createElement("div");
+    searchBarAndGlass.setAttribute("class", "search-bar-and-glass")
+    ingredientsSearch.appendChild(searchBarAndGlass);
+
+    const searchBarIngredients = document.createElement("input");
+    searchBarIngredients.setAttribute("type", "search");
+    searchBarAndGlass.appendChild(searchBarIngredients);
+
+    const searchBarGlass = document.createElement("i");
+    searchBarGlass.setAttribute("class", "fa-solid fa-magnifying-glass");
+    searchBarAndGlass.appendChild(searchBarGlass)
+
+    const ingredientsList = document.createElement("ul");
+    ingredientsSearch.appendChild(ingredientsList);
+
+    const ingredientsSet = new Set();
+
+    recipesData.forEach((recipes) => {
+        recipes.ingredients.forEach((ingredients) => {
+            ingredientsSet.add(ingredients.ingredient);
         });
     });
-    
-    ingredientContainer.appendChild(buttonIngredients)
-    
-}
+
+    ingredientsSet.forEach((ingredient) => {
+        const ingredientsLi = document.createElement("li");
+        ingredientsLi.textContent = ingredient;
+        ingredientsList.appendChild(ingredientsLi);
+    });
+
+    ingredientContainer.appendChild(buttonIngredients);
+};
