f8d8cf338e9e3793c1d1c99e2c0df926c7662b81
Walid <walid.bensalah20@gmail.com>
2025-03-17 14:51:10 +0100

    Algo de recherche methode 1 terminé

--- scripts/utils/searchalgo.js
+++ scripts/utils/searchalgo.js
@@ -1,6 +1,22 @@
 export const searchRecipe = (text, recipes) => {
+    const lowerText = text.toLowerCase(); // convertir le texte en minuscules
+
     return recipes.filter(recipe => {
-        return recipe.name.toLowerCase().includes(text.toLowerCase()); 
+        // nom rectte
+        const nameMatch = recipe.name.toLowerCase().includes(lowerText);
+
+        // ingrédients
+        const ingredientMatch = recipe.ingredients.some(ingredient => //La méthode some() teste si au moins un élément du tableau passe le test implémenté par la fonction fournie. Elle renvoie un booléen indiquant le résultat du test.
+            ingredient.ingredient.toLowerCase().includes(lowerText)
+        );
+
+        // ustensiles
+        const ustensilMatch = recipe.ustensils.some(ustensil =>
+            ustensil.toLowerCase().includes(lowerText)
+        );
+
+        // retourne true si l'un des critères correspond
+        return nameMatch || ingredientMatch || ustensilMatch;
     });
 };
 
